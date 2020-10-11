e90c6cec61831e265b8cef70a5732c206e0180b8
Alejandro <carlosaguirre7@hotmail.com>
2020-10-11 16:17:51 -0500

    Map locations implemented

---
+++ bgweather/MapVC.swift
@@ -0,0 +1,103 @@
+//
+//  MapVC.swift
+//  bgweather
+//
+//  Created by Alejandro Agudelo on 11/10/20.
+//  Copyright © 2020 Alejandro Agudelo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapVC: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet var longPressGesture: UILongPressGestureRecognizer!
+    
+    var listCoordinates = [CLLocationCoordinate2D]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func longPressDetected(_ sender: UILongPressGestureRecognizer) {
+        if sender.state != UIGestureRecognizer.State.began {
+            return
+        }
+        
+        // Get the coordinates of the point you pressed long.
+        let location = sender.location(in: mapView)
+        
+        // Convert location to CLLocationCoordinate2D.
+        let myCoordinate: CLLocationCoordinate2D = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        // Add to list of coordinates
+        listCoordinates.append(myCoordinate)
+        
+        // Generate pins.
+        let myPin: MKPointAnnotation = MKPointAnnotation()
+        
+        // Set the coordinates.
+        myPin.coordinate = myCoordinate
+        
+        // Added pins to MapView.
+        mapView.addAnnotation(myPin)
+    }
+    
+}
+
+/*
+class _06: BaseViewController {
+    
+    // Generate MKMapView
+    lazy var _mapView: MKMapView = {
+        let mv = MKMapView(frame: self.view.frame)
+        
+        // Set a delegate.
+        mv.delegate = self
+        
+        // Designate center point.
+        let center: CLLocationCoordinate2D = CLLocationCoordinate2DMake(kDefault_latitude, kDefault_longitude)
+        
+        // Set center point in MapView.
+        mv.setCenter(center, animated: true)
+        
+        // Specify the scale (display area).
+        let mySpan: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMake(center, mySpan)
+        
+        // Add region to MapView.
+        mv.region = myRegion
+        
+    }()
+}
+*/
+extension MapVC: MKMapViewDelegate {
+    
+    // Delegate method called when addAnnotation is done.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let myPinIdentifier = "PinAnnotationIdentifier"
+        
+        // Generate pins.
+        let myPinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: myPinIdentifier)
+        
+        // Add animation.
+        myPinView.animatesDrop = true
+        
+        // Default pin color
+        myPinView.pinTintColor = .blue
+        
+        // Display callouts.
+        myPinView.canShowCallout = true
+        
+        // Set annotation.
+        myPinView.annotation = annotation
+        
+        print("latitude: \(annotation.coordinate.latitude), longitude: \(annotation.coordinate.longitude)")
+        
+        return myPinView
+    }
+}
+
