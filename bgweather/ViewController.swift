ee513a67bba3b7a0968b3b11040ca0b83fc2bd7c
Alejandro <carlosaguirre7@hotmail.com>
2020-10-11 11:24:12 -0500

    Initial Commit

---
+++ bgweather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bgweather
+//
+//  Created by Alejandro Agudelo on 11/10/20.
+//  Copyright © 2020 Alejandro Agudelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
